48086d7c21231f788882113eb34704755f55f000
Davis Too <33203305+kipkiruitoo@users.noreply.github.com>
2020-09-06 16:05:29 +0300

    forgot what i did

--- app/Containers/User/UI/API/Routes/searchUserByQrCode.v1.private.php
+++ app/Containers/User/UI/API/Routes/searchUserByQrCode.v1.private.php
@@ -4,12 +4,13 @@
  * @apiGroup           User
  * @apiName
  *
- * @api                {GET} /v1/users?search=qr_string:qrcode Find User By Qr Code..
- * @apiDescription     search for user details using qr code
+ * @api                {GET} /v1/users Endpoint title here..
+ * @apiDescription     Endpoint description here..
  *
  * @apiVersion         1.0.0
- * @apiPermission      Authenticated User
+ * @apiPermission      none
  *
+ * @apiParam           {String}  parameters here..
  *
  * @apiSuccessExample  {json}  Success-Response:
  * HTTP/1.1 200 OK
@@ -17,3 +18,12 @@
   // Insert the response of the request here...
 }
  */
+
+/** @var Route $router */
+$router->get('users', [
+  'as' => 'api_user_',
+  'uses'  => 'Controller@',
+  'middleware' => [
+    'auth:api',
+  ],
+]);
